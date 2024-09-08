41a9a3f39b9900a8e31b1c1e6b67ccb7b0761252
akasatana12321 <kantayiteng168@gmail.com>
2024-09-08 20:15:42 +0900

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,17 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.tab-button');
-    const contents = document.querySelectorAll('.tab-content');
+document.querySelectorAll('.tab-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const tabId = this.dataset.tab;
 
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const tab = button.getAttribute('data-tab');
+        document.querySelectorAll('.tab-content').forEach(content => {
+            content.classList.remove('active');
+        });
 
-            contents.forEach(content => {
-                content.classList.add('hidden');
-                if (content.id === tab) {
-                    content.classList.remove('hidden');
-                }
-            })
-        })
-    })
-})
+        document.getElementById(tabId).classList.add('active');
+    });
+});
