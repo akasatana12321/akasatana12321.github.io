d3544da2e140b649b7a34ca304790ec9d6b1f3a5
akasatana12321 <kantayiteng168@gmail.com>
2024-09-08 22:39:35 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function showContent(sectionId) {
+    // 全てのセクションを隠す
+    var sections = document.querySelectorAll('.content');
+    sections.forEach(function(section) {
+        section.classList.add('hidden');
+        section.classList.remove('active');
+    });
+
+    // 選択されたセクションを表示
+    var activeSection = document.getElementById(sectionId);
+    activeSection.classList.remove('hidden');
+    activeSection.classList.add('active');
+}
