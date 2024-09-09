a1f63e1d486f18e1c50d232b4d9c23f5463e5017
akasatana12321 <kantayiteng168@gmail.com>
2024-09-09 12:43:16 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -23,3 +23,51 @@ window.onload = function() {
         showContent('APEX');
     }
 };
+
+function returnTovalo() {
+    if (window.location.hash === '#valorant') {
+        showContent('valorant');
+    }
+}
+
+window.onload = function() {
+    if (window.location.hash === '#valorant') {
+        showContent('valorant');
+    }
+};
+
+function returnToStreet() {
+    if (window.location.hash === '#Street') {
+        showContent('Street');
+    }
+}
+
+window.onload = function() {
+    if (window.location.hash === '#Street') {
+        showContent('Street');
+    }
+};
+
+function returnToOW2() {
+    if (window.location.hash === '#OW2') {
+        showContent('OW2');
+    }
+}
+
+window.onload =function() {
+    if (window.location.hash === '#OW2') {
+        showContent('OW2');
+    }
+};
+
+function returnToLoL() {
+    if (window.location.hash === '#LoL') {
+        showContent('LoL');
+    }
+}
+
+window.onload = function() {
+    if (window.location.hash === '#LoL') {
+        showContent('LoL');
+    }
+};  
